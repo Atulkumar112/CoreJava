569357f4f0117a45d080b954b66e61fbb6eb47f8
Atul kumar <57915384+Atulkumar112@users.noreply.github.com>
2023-03-10 01:02:12 +0530

    Create All About Constructor.java

---
+++ All About Constructor.java
@@ -0,0 +1,4 @@
+https://www.programiz.com/java-programming/constructors#:~:text=In%20Java%2C%20constructors%20can%20be,Default%20Constructor
+
+just visit above link, it have everything about constructor 
+java have 3 types constructor. just go through the link to learn more!
